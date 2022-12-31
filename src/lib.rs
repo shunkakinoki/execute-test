948313196e99020c0edb6f0733f53eb40a7baa60
Shun Kakinoki <shunkakinoki@gmail.com>
2022-12-30 17:10:55 -0800

    feat: ini token name and symbol

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 mod config;
 use anyhow::{Ok, Result};
-use bytes::Bytes;
 pub use config::NodeConfig;
-use ethers::types::{H160, U256};
+use ethers::{
+    abi::Detokenize,
+    types::{Log, H160, H256, U256},
+};
 use foundry_evm::executor::{fork::CreateFork, opts::EvmOpts, Backend, Executor, ExecutorBuilder};
+use futures::future::join_all;
 use std::str::FromStr;
 
 pub fn spawn(config: NodeConfig) -> Executor {
@@ -22,12 +25,46 @@ pub fn spawn(config: NodeConfig) -> Executor {
     return executor;
 }
 
+pub async fn resolve_call_args<D: Detokenize>(args: &[String], executor: Executor) -> Vec<D> {
+    join_all(args.iter().map(|arg| async {
+        executor
+            .call(
+                H160::from_str("0x4fd9D0eE6D6564E80A9Ee00c0163fC952d0A45Ed").unwrap(),
+                H160::from_str("0x04F2694C8fcee23e8Fd0dfEA1d4f5Bb8c352111F").unwrap(),
+                arg.clone(),
+                (),
+                0.into(),
+                None,
+            )
+            .unwrap()
+            .result
+    }))
+    .await
+}
+
 pub async fn simulate(mut executor: Executor) -> Result<()> {
-    let _ = executor.call_raw_committing(
-        H160::from_str("0x4fd9D0eE6D6564E80A9Ee00c0163fC952d0A45Ed").unwrap(),
-        H160::from_str("0x225E9B54F41F44F42150b6aAA730Da5f2d23FAf2").unwrap(),
-        Bytes::from(""),
-        U256::from(0),
-    );
+    let res = executor
+        .call_raw_committing(
+            H160::from_str("0x4fd9D0eE6D6564E80A9Ee00c0163fC952d0A45Ed").unwrap(),
+            H160::from_str("0x04F2694C8fcee23e8Fd0dfEA1d4f5Bb8c352111F").unwrap(),
+            hex::decode("a9059cbb000000000000000000000000225e9b54f41f44f42150b6aaa730da5f2d23faf2000000000000000000000000000000000000000000000000000000003b9aca00").expect("valid").into(),
+            U256::zero(),
+        )
+        .unwrap();
+
+    if res.logs.iter().any(|log: &Log| {
+        return log.topics.contains(
+            &H256::from_str("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef")
+                .unwrap(),
+        );
+    }) {
+        let c = [String::from("name()(string)"), String::from("symbol()(string)")];
+
+        let results = resolve_call_args::<String>(&c, executor.clone()).await;
+
+        println!("Token name: {:#?}", results.first().unwrap());
+        println!("Token symbol: {:#?}", results.get(results.len().wrapping_sub(1)).unwrap());
+    }
+
     Ok(())
 }
