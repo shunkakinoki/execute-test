3d6b268c7563467555e836e5c62865a25d4ba5de
Shun Kakinoki <shunkakinoki@gmail.com>
2022-12-30 17:00:07 -0800

    feat: ini src

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 mod config;
 use anyhow::{Ok, Result};
+use bytes::Bytes;
 pub use config::NodeConfig;
 use ethers::types::{H160, U256};
 use foundry_evm::executor::{fork::CreateFork, opts::EvmOpts, Backend, Executor, ExecutorBuilder};
 use std::str::FromStr;
 
-pub async fn spawn(config: NodeConfig) -> Executor {
+pub fn spawn(config: NodeConfig) -> Executor {
     let evm_opts = EvmOpts { fork_url: Some(config.fork_url.clone()), ..Default::default() };
 
     let env = evm_opts.evm_env_blocking().unwrap();
@@ -20,15 +21,5 @@ pub async fn spawn(config: NodeConfig) -> Executor {
 }
 
 pub async fn simulate(mut executor: Executor) -> Result<()> {
-    let token_res = executor
-        .call_raw_committing(
-            H160::from_str("0x4fd9D0eE6D6564E80A9Ee00c0163fC952d0A45Ed").unwrap(),
-            H160::from_str("0x04F2694C8fcee23e8Fd0dfEA1d4f5Bb8c352111F").unwrap(),
-            hex::decode("a9059cbb000000000000000000000000225e9b54f41f44f42150b6aaa730da5f2d23faf2000000000000000000000000000000000000000000000000000000003b9aca00").expect("valid").into(),
-            U256::zero(),
-        )
-        .unwrap();
-    println!("{:?}", token_res);
-
     Ok(())
 }
